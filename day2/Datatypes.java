c8c23727f888e7d2756ec1ba573445d0d0f247ef
SwathiSasikumar24 <133939155+SwathiSasikumar24@users.noreply.github.com>
2023-05-20 15:50:49 +0530

    Add files via upload
    Week1Day2

---
+++ day2/Datatypes.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 31;
+		boolean isVaccinated = true;
+		String name ="Swathi";
+		System.out.println(age);
+		System.out.println(isVaccinated);
+		System.out.println(name);
+
+	}
+
+}
