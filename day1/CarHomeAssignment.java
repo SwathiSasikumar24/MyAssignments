e17c25f62a44287733f9345e682fc288d01d935d
SwathiSasikumar24 <133939155+SwathiSasikumar24@users.noreply.github.com>
2023-05-27 12:51:54 +0530

    Add files via upload
    car home assignment

---
+++ day1/CarHomeAssignment.java
@@ -0,0 +1,38 @@
+package week1.day1;
+
+public class CarHomeAssignment 
+{
+	private void driveCar() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("driver Car");
+	}
+	private void applyBrake() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("ApplyBrake");
+	}
+	private void soundHorn() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("soundHorn");
+
+	}
+	private void isPuncture() 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("isPuncture");
+
+	}
+
+public static void main(String[] args) {
+
+		CarHomeAssignment car1 = new CarHomeAssignment();
+		car1.driveCar();
+		car1.applyBrake();
+		car1.isPuncture();
+		car1.soundHorn();
+
+	}
+}
+
