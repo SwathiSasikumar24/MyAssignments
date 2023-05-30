b381aba353fa9a4c899ab2a9c697813e51e0c707
SwathiSasikumar24 <133939155+SwathiSasikumar24@users.noreply.github.com>
2023-05-30 18:12:14 +0530

    Add files via upload
    2 files

---
+++ day2/Assignment5.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class Assignment5 {
+
+	public static void main(String[] args) {
+
+		ClassStudent cs = new ClassStudent();
+		System.out.println(cs.studentName);
+		System.out.println(cs.collegeName);
+		System.out.println(cs.rollNo);
+		System.out.println(cs.markScored); 
+		System.out.println(cs.cgpa); 
+		
+	}
+
+}
---
+++ day2/ClassStudent.java
@@ -0,0 +1,11 @@
+package week1.day2;
+
+public class ClassStudent {
+	// TODO Auto-generated method stub
+	String studentName = "Swathi";
+	int rollNo = 12345;
+	String collegeName ="MCW";
+	int markScored =980;
+	float cgpa = 9.8f;
+
+}
